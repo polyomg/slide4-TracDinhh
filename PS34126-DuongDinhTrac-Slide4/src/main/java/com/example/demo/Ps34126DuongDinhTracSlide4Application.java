d349ff31cfb8ffdeb765a229dcfb9bac2406c062
Dương Đình Trác - Lab1 <duongdinhtrac0103@gmail.com>
2025-10-02 10:53:24 +0700

    PS34126_DuongDinhTrac_Slide4_First commit

---
+++ PS34126-DuongDinhTrac-Slide4/src/main/java/com/example/demo/Ps34126DuongDinhTracSlide4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps34126DuongDinhTracSlide4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps34126DuongDinhTracSlide4Application.class, args);
+	}
+
+}
